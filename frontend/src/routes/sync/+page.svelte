afccba589bb6838a13632732ff77a2a19440ca9a
zachgourmetnexus <ztrow787@gmail.com>
2023-11-23 19:03:20 -0500

    added custom text functionallity

--- frontend/src/routes/sync/+page.svelte
+++ frontend/src/routes/sync/+page.svelte
@@ -1,15 +1,13 @@
 <script lang="ts">
   import { onMount } from 'svelte';
   import axios from 'axios';
-  import RangeSlider from "svelte-range-slider-pips";
 
   let file: File | null = null;
   let gif: Blob | null = null;
   let loading = false;
   let uploadProgress = 0;
-  let imageDistortionMinMax = [10,40];
   let matchedFilename = "";
-  let fps = [24];
+  let textFromAudio = "";
   
   const handleFileChange = (event: any) => {
     file = event.target.files[0];
@@ -19,16 +17,17 @@
   };
   
   const handleSubmit = async (event: any) => {
-    gif = null;
     if (!file) {
       return;
     }
-  
+    gif = null;
+
     loading = true;
-    (event.target as HTMLInputElement).style.backgroundImage = 'upload.jpeg';
+    // (event.target as HTMLInputElement).style.backgroundImage = 'upload.jpeg';
     const formData = new FormData();
     formData.append('audio_file', file);
-    formData.append('auto_text', true);
+    formData.append('auto_text', textFromAudio? false : true);
+    formData.append('text', textFromAudio);
     // formData.append('fps', fps[0].toString());
     // formData.append('min', imageDistortionMinMax[0].toString());
     // formData.append('max', imageDistortionMinMax[1].toString());
@@ -48,6 +47,7 @@
       matchedFilename = matchResult ? matchResult[1] : '';
       console.log(matchedFilename);
       console.log(response, response.config, response.data.filename);
+      file = file;
     } catch (error) {
       console.error(error);
     }
@@ -64,7 +64,7 @@
       align-items: center;
     }
   
-    input[type="file"] {
+    /* input[type="file"] {
       margin: 20px;
       border: dashed;
       cursor: pointer;
@@ -77,7 +77,7 @@
     input[type="file"]::-webkit-file-upload-button {
       visibility: hidden;
       display: none;
-    }
+    } */
 
     progress {
       justify-self: center;
@@ -124,34 +124,30 @@
       }
     }
 
+    .iframe {
+      width: 100%;
+      min-height: 500px;
+      height: 100%;
+      border: none;
+    }
   </style>
 
   <div id="parent">
-    <form on:submit|preventDefault={handleSubmit}>
+    <form on:submit={handleSubmit}>
       {#if loading}
         <progress value={uploadProgress} max="100"><div>{uploadProgress}%</div></progress>
         {:else}
           <input id="fileInput" type="file" on:change={handleFileChange} class:loading={loading}/> 
-          
-          <div>
-            <b>MIN {imageDistortionMinMax[0]} | MAX {imageDistortionMinMax[1]}</b>
-            <RangeSlider id=small bind:values={imageDistortionMinMax} range float pips all="label" step={10} />
-          </div>
-
-          <div>
-            <b>FPS: {fps[0]}</b>
-            <RangeSlider id=small values:{24} bind:values={fps} range float pips max={30} step={1} />
-          </div>
-
+          <textarea id="textArea" bind:value={textFromAudio}></textarea>
           <button id="submit" type="submit" disabled={loading}>Generate GIF</button>
       {/if}
     </form>
       {#if gif}
         <video width= "320" height="240" controls>
           <source src={URL.createObjectURL(gif)} type="video/webm">
-            </video>
+        </video>
 
-          	<iframe title="blah" src={"http://localhost:8003/transcriptions//" + matchedFilename}></iframe>
+        <iframe class="iframe" title="blah" src={"http://localhost:8003/transcriptions//" + matchedFilename}></iframe>
       {/if}
   </div>
   
